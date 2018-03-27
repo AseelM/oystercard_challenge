4062b07e7650144c1bd4a6a14fc2bce3a1ebbb89
aseelm <aseelmustafa@outlook.com>
2018-03-27 17:35:32 +0100

    enables topup and raises error when balance limit is exceeded

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,16 @@
 class Oystercard
 
   attr_reader :balance
+  LIMIT_VALUE = 90
+  LIMIT_ERROR = "your balance will exceed #{LIMIT_VALUE}"
 
   def initialize (balance = 0)
     @balance = balance
   end
 
   def top_up(credit)
+    raise LIMIT_ERROR  if (@balance + credit) > LIMIT_VALUE
     @balance += credit
   end
-  
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,5 +8,12 @@ describe Oystercard do
 
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
+  it "returns balance if within limit" do
+    expect(subject.top_up(80)).to eq(80)
+  end
+
+  it "raises error" do
+    expect { subject.top_up(91) }.to raise_error described_class::LIMIT_ERROR
+  end
 
 end
